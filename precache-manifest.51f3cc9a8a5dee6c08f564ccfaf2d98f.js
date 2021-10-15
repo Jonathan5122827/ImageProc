f68e648817c81f75e178564af3d8d7c4aaf3f431
Jonathan5122827 <jonathan5122827@gmail.com>
2021-10-15 18:14:59 -0500

    Updates

---
+++ precache-manifest.51f3cc9a8a5dee6c08f564ccfaf2d98f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2239f7de9ba40fa0d0",
+    "url": "/ImageProc/static/js/runtime~main.0c2239f7.js"
+  },
+  {
+    "revision": "bbe9a8d42fe7579d6126",
+    "url": "/ImageProc/static/js/main.bbe9a8d4.chunk.js"
+  },
+  {
+    "revision": "357814436f44ca4dda4f",
+    "url": "/ImageProc/static/js/2.35781443.chunk.js"
+  },
+  {
+    "revision": "f106087f2fd628e526cce772f34f9d2f",
+    "url": "/ImageProc/index.html"
+  }
+];
